0fe1e4e1b594434548c663f7481b61b347027568
Archibong Victor <varchibong428@gmail.com>
2026-05-06 10:07:55 -0500

    Updated the comments for the add to cart function

--- src/js/product.js
+++ src/js/product.js
@@ -9,6 +9,7 @@ function addProductToCart(product) {
   // get existing cart from local storage or initialize empty array
   let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
 
+  // add new products to the cart for each time a user clicks the add to cart button. 
   // add new products to the cart for each time a user clicks the add to cart button.
   cart.push(product);
 
