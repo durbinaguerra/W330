2ad1cce5fc24529a116e24570d226ed038b8de4f
Dianauguerra <diana.1609@hotmail.com>
2026-05-07 08:43:34 -0600

    my branch

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,17 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // get existing cart from local storage or initialize empty array
+  let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+
+  // add new product
+  cart.push(product);
+  
+  // save updated cart
+  localStorage.setItem("so-cart", JSON.stringify(cart));
+
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
