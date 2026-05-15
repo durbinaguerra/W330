2cc0afb768c5eb88665f1c9fd45cacd9cdcee1bd
Dianauguerra <diana.1609@hotmail.com>
2026-05-14 18:56:56 -0600

    step 5.2

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,3 +1,15 @@
+// ProductList.mjs
+function productCardTemplate(product) {
+  return `<li class="product-card">
+    <a href="product_pages/?product=">
+      <img src="" alt="Image of ">
+      <h2 class="card__brand"></h2>
+      <h3 class="card__name"></h3>
+      <p class="product-card__price">$</p>
+    </a>
+  </li>`
+}
+
 
 export default class ProductList {
   constructor(dataSource) {
