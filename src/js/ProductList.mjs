e6db49b4ec24f2d7b888f54bd48050ff2015f6d9
Dianauguerra <diana.1609@hotmail.com>
2026-05-14 19:03:06 -0600

    step 6.1

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,13 +1,16 @@
 // ProductList.mjs
 function productCardTemplate(product) {
   return `<li class="product-card">
-    <a href="product_pages/?product=">
-      <img src="" alt="Image of ">
-      <h2 class="card__brand"></h2>
-      <h3 class="card__name"></h3>
-      <p class="product-card__price">$</p>
+    <a href="product_pages/index.html?product=${product.Id}">
+      <img 
+        src="${product.Image}" 
+        alt="Image of ${product.Name}"
+      >
+      <h2 class="card__brand">${product.Brand.Name}</h2>
+      <h3 class="card__name">${product.Name}</h3>
+      <p class="product-card__price">$${product.FinalPrice}</p>
     </a>
-  </li>`
+  </li>`;
 }
 
 
