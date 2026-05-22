6b79a91a1856d50cb609aed497b3169cbc1fef21
Dianauguerra <diana.1609@hotmail.com>
2026-05-21 21:54:14 -0600

    nvm

--- src/js/main.js
+++ src/js/main.js
@@ -7,14 +7,4 @@ const element = document.querySelector(".product-list");
 
 const productList = new ProductList("Tents", dataSource, element);
 
-
-import class ProductList {
-  constructor(category, dataSource, listElement) {
-    // You passed in this information to make the class as reusable as possible.
-    // Being able to define these things when you use the class will make it very flexible
-    this.category = category;
-    this.dataSource = dataSource;
-    this.listElement = listElement;
-  }
-
 productList.init();
